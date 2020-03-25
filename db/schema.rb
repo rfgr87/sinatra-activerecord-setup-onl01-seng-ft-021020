b353589cbde1a3566b8ab4a9e989f996653be6b7
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-25 19:12:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_25_184504) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+  end
 
 end
